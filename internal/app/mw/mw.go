ce21b58bd313eb906ce6c6c48419e8ca28741f26
Dauren Shalabayev <dauren.shalabayev@gmail.com>
2023-09-14 19:31:45 +0600

    Finish

---
+++ internal/app/mw/mw.go
@@ -0,0 +1,26 @@
+package mw
+
+import (
+	"log"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+const roleAdmin = "admin"
+
+func RoleCheck(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		val := c.Request().Header.Get("User-Role")
+		if strings.EqualFold(val, roleAdmin) {
+			log.Println("red user button detected")
+		}
+
+		err := next(c)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
